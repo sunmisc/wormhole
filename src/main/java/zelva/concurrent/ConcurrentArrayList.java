20840e35f8d8c28301542460d5eba5b254c2085b
Zelva <jolyjdia@gmail.com>
2021-11-20 04:21:56 +0300

    whoa?

--- src/main/java/zelva/concurrent/ConcurrentArrayList.java
+++ src/main/java/zelva/concurrent/ConcurrentArrayList.java
@@ -2,10 +2,11 @@ package zelva.concurrent;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
+import java.util.Arrays;
 import java.util.concurrent.atomic.LongAdder;
 
 public class ConcurrentArrayList<E> {
-    volatile Node<E>[] table = new Node[16]; // test
+    volatile Node<E>[] table = new Node[1]; // test
     final LongAdder adder = new LongAdder();
 
     public Object add(E e) {
@@ -13,6 +14,28 @@ public class ConcurrentArrayList<E> {
         Object o = set(e, i);
         addCount(1);
         return o;
+    }
+    public E get(int i) {
+        Node<E> f = tabAt(table, i);
+        if (f instanceof MovedNode) {
+            f = tabAt(((MovedNode<E>)f).next, i);
+        }
+        return f.val;
+    }
+    public E remove(int i) {
+        Node<E>[] tab;
+        for (Node<E> f;;) {
+            if ((f = tabAt(tab = table, i)) == null) {
+                return null;
+            } else if (f instanceof MovedNode) {
+                tab = ((MovedNode<E>) f).next;
+                f = tabAt(tab, i);
+            }
+            if (f != null) {
+
+            }
+            return null;
+        }
     }
 
     private void addCount(int c) {
@@ -26,7 +49,7 @@ public class ConcurrentArrayList<E> {
 
     @SuppressWarnings("unchecked")
     static <E> Node<E> tabAt(Node<E>[] tab, int i) {
-        return (Node<E>) AA.getAcquire(tab, i);
+        return (Node<E>) AA.getVolatile(tab, i); // acquire
     }
 
     static <E> boolean casTabAt(Node<E>[] tab, int i, Node<E> c, Node<E> v) {
@@ -38,17 +61,20 @@ public class ConcurrentArrayList<E> {
     }
 
     @SuppressWarnings("unchecked")
-    static <E> Node<E> getAndSetTabAt(Node<E>[] tab, int i,  Node<E> v) {
+    static <E> Node<E> getAndSetTabAt(Node<E>[] tab, int i, Node<E> v) {
         return (Node<E>) AA.getAndSet(tab, i, v);
     }
 
     static <E> void setTabAt(Node<E>[] tab, int i, Node<E> v) {
-        AA.setRelease(tab, i, v);
+        AA.setVolatile(tab, i, v); // release
     }
     public Object set(E e, int i) {
-        Node<E>[] tab;
+        Node<E>[] tab = table;
+        int len;
+        if (i > (len = tab.length)-1)
+            tab = resize(tab, len);
         for (Node<E> f;;) {
-            if ((f = tabAt(tab = table, i)) == null) { // если еще ни разу не увеличивал
+            if ((f = tabAt(tab, i)) == null) {
                 if (casTabAt(tab, i, null, new Node<>(e))) {
                     break;
                 } else {
@@ -63,19 +89,23 @@ public class ConcurrentArrayList<E> {
             f.val = e;
             break;
         }
-        int len;
-        if (i >= (len = tab.length)-1)
-            resize(tab, len);
         return tab;
     }
 
     public Node<E>[] resize(Node<E>[] tab, int len) {
         Node<E>[] next = new Node[len << 1];
         MovedNode<E> movedNode = new MovedNode<>(next);
-
-        for (int i = 0; i < len; ++i) {
-            Node<E> f = getAndSetTabAt(tab, i, movedNode);
-            next[i] = f;
+        outer: for (int i = 0; i < len; ++i) {
+            for (Node<E> f;;) {
+                if ((f = tabAt(tab, i))
+                        instanceof MovedNode) {
+                    continue outer;
+                }
+                setTabAt(next, i, f); // volatile ?
+                if (casTabAt(tab, i, f, movedNode)) {
+                    continue outer;
+                }
+            }
         }
         return table = next;
     }
@@ -98,9 +128,27 @@ public class ConcurrentArrayList<E> {
             super(null);
             this.next = next;
         }
+        @Override
+        public String toString() {
+            return "moved";
+        }
     }
     public Node<E>[] getTable() {
         return table;
+    }
+
+    public String getTableStr() {
+        Node<E>[] tab = table;
+        int iMax =  tab.length - 1;
+        StringBuilder b = new StringBuilder();
+        b.append('[');
+        for (int i = 0; ; i++) {
+            b.append(tabAt(tab, i));
+            if (i == iMax)
+                return b.append(']').toString();
+            b.append(", ");
+        }
+
     }
     // VarHandle mechanics
     private static final VarHandle AA
