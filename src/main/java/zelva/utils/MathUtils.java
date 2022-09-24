4a423fceb9b6e0405744053f57451115aa6f48d8
Sunmisc <zelvalea@gmail.com>
2022-09-24 21:21:05 +0300

    updated MathUtils

--- src/main/java/zelva/utils/MathUtils.java
+++ src/main/java/zelva/utils/MathUtils.java
@@ -7,10 +7,11 @@ package zelva.utils;
 public final class MathUtils {
     public static final double _2PI         = 2 * Math.PI;
 
-    private static final int MAX_PRECISION = (1 << 16);
+    private static final int MAX_PRECISION  = (1 << 16);
+    private static final int HALF_PRECISION = MAX_PRECISION >> 1;
     private static final int _N             = MAX_PRECISION - 1;
     private static final int _COS           = MAX_PRECISION >> 2;
-    private static final double _H          = MAX_PRECISION /_2PI;
+    private static final double _H          = MAX_PRECISION / _2PI;
 
     private static final int[] SINE_TABLE_INT = new int[(1 << 14) + 1];
 
@@ -44,24 +45,23 @@ public final class MathUtils {
 
 
     private static float lookup(int index) {
-        int half = MAX_PRECISION >> 1;
 
         // Trigonometric identity: sin(-x) = -sin(x)
         // Given a domain of 0 <= x <= 2*pi, just negate the value if x > pi.
         // This allows the sin table size to be halved.
 
-        int neg = (index & half) << 16;
+        int neg = (index & HALF_PRECISION) << 16;
 
         // All bits set if (pi/2 <= x), none set otherwise
         // Extracts the 15th bit from 'half'
         int mask = (index << 17) >> 31;
 
         // Trigonometric identity: sin(x) = sin(pi/2 - x)
-        int pos = ((half + 1) & mask) + (index ^ mask);
+        int pos = ((HALF_PRECISION + 1) & mask) + (index ^ mask);
 
         // Wrap the position in the table. Moving this down to immediately before the array access
         // seems to help the Hotspot compiler optimize the bit math better.
-        pos &= (half - 1);
+        pos &= (HALF_PRECISION - 1);
 
         // Fetch the corresponding value from the LUT and invert the sign bit as needed
         // This directly manipulate the sign bit on the float bits to simplify logic
