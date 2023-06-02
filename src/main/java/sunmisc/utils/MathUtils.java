3301d9b407aa5f5d0c896f4dec1da131259625cf
ZelvaLea <zelvalea@gmail.com>
2023-06-02 22:53:13 +0300

    author renamed and cleanup

--- src/main/java/sunmisc/utils/MathUtils.java
+++ src/main/java/sunmisc/utils/MathUtils.java
@@ -2,26 +2,19 @@ package sunmisc.utils;
 
 
 /**
- * @author ZelvaLea
+ * @author Sunmisc Unsafe
  */
 public final class MathUtils {
-    public static final double _2PI         = 2 * Math.PI;
 
     private static final int PRECISION      = 1 << 16;
-    private static final int HALF           = PRECISION >> 1;
-    private static final int _N             = PRECISION - 1;
-    private static final int QUARTER        = PRECISION >> 2;
-    private static final double _H          = PRECISION / _2PI;
 
-    private static final int[] SINE_TABLE_INT = new int[QUARTER + 1];
+    private static final int[] SINE_TABLE_INT;
 
     static {
-        /**
-         * @author coderbot16   Author of the original (and very clever) implementation in Rust:
-         *  <a href="https://gitlab.com/coderbot16/i73/-/tree/master/i73-trig/src">...</a>
-         */
-        final double delta = _2PI / PRECISION;
-        for (int i = 0, len = SINE_TABLE_INT.length; i < len; i++) {
+        int n = (PRECISION >>> 2) + 1;
+        SINE_TABLE_INT = new int[n];
+        double delta = Math.TAU / PRECISION;
+        for (int i = 0; i < n; i++) {
             float f = (float) Math.sin(i * delta);
             SINE_TABLE_INT[i] = Float.floatToRawIntBits(f);
         }
@@ -29,43 +22,45 @@ public final class MathUtils {
 
     private MathUtils() {}
 
+    public static int sumArithmeticProgression(int n) {
+        int y = n + 1;
+        return (y & 1) == 0 ? n * (y >> 1) : (n >> 1) * y;
+    }
 
     public static float _sin(double value) {
-        return lookup(((int)(value*_H)));
+        double h = PRECISION / Math.TAU;
+        return lookup((int)(value*h));
     }
     public static float _cos(double value) {
-        return lookup(((int)(value*_H+QUARTER)));
+        double h = PRECISION / Math.TAU, q = PRECISION >>> 2;
+        return lookup((int)(value *h + q));
     }
 
 
     private static float lookup(int index) {
-        index &= _N;
+        index &= (PRECISION - 1);
+
+        int h = PRECISION >> 1;
         // Trigonometric identity: sin(-x) = -sin(x)
         // Given a domain of 0 <= x <= 2*pi, just negate the value if x > pi.
         // This allows the sin table size to be halved.
 
-        int neg = (index & HALF) << 16;
+        int neg = (index & h) << 16;
 
         // All bits set if (pi/2 <= x), none set otherwise
         // Extracts the 15th bit from 'half'
         int mask = (index << 17) >> 31;
 
         // Trigonometric identity: sin(x) = sin(pi/2 - x)
-        int pos = ((HALF + 1) & mask) + (index ^ mask);
+        int pos = ((h + 1) & mask) + (index ^ mask);
 
         // Wrap the position in the table. Moving this down to immediately before the array access
         // seems to help the Hotspot compiler optimize the bit math better.
-        pos &= (HALF - 1);
+        pos &= (h - 1);
 
         // Fetch the corresponding value from the LUT and invert the sign bit as needed
         // This directly manipulate the sign bit on the float bits to simplify logic
         return Float.intBitsToFloat(SINE_TABLE_INT[pos] ^ neg);
     }
 
-    public static int sumArithmeticProgression(int n) {
-        int y = n + 1;
-        return (y & 1) == 0
-                ? n * (y >> 1)
-                : (n >> 1) * y;
-    }
 }
