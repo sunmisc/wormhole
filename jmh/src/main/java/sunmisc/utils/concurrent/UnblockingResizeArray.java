0418cfa9ce7c9c31145daf123b1e5d520aed3214
Sunmisc Unsafe <jolyjdia@.gmail.com>
2023-10-27 17:50:07 +0300

    cleanup

--- jmh/src/main/java/sunmisc/utils/concurrent/UnblockingResizeArray.java
+++ jmh/src/main/java/sunmisc/utils/concurrent/UnblockingResizeArray.java
@@ -19,24 +19,20 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Threads(1)
 @Fork(1)
 public class UnblockingResizeArray {
-    static final double SC = Math.PI * 5;
-    static final int DELTA = 700_000;
+    private static final double SC = Math.PI * 5;
+    private static final int DELTA = 700_000;
     private UnblockingArrayBuffer<Integer> nextGen;
-    private UnblockingArrayBufferOld<Integer> pastGen;
 
-    private AtomicInteger a, b;
+    private AtomicInteger a;
 
 
     @Setup
     public void prepare() {
         nextGen = new UnblockingArrayBuffer<>(DELTA);
-        pastGen = new UnblockingArrayBufferOld<>(DELTA);
         a = new AtomicInteger(1);
-        b = new AtomicInteger(1);
 
         for (int i = 0; i < DELTA; ++i) {
             nextGen.put(i,i);
-            pastGen.put(i,i);
         }
     }
 
@@ -44,12 +40,6 @@ public class UnblockingResizeArray {
     public int resizeNextGen() {
         int x = getNextSize(a.getAndIncrement());
         nextGen.resize(r -> x);
-        return x;
-    }
-    @Benchmark
-    public int resizePastGen() {
-        int x = getNextSize(b.getAndIncrement());
-        pastGen.resize(r -> x);
         return x;
     }
     static int getNextSize(int a) {
